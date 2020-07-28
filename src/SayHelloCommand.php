235d831b50537027ca8c3930747364baacec92a4
Aleksey Gulko <dev.gulkoalexey@gmail.com>
2020-07-28 17:45:05 +0300

    delete unusual coments

--- src/SayHelloCommand.php
+++ src/SayHelloCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class SayHelloCommand extends Command
 {
-    // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:say-hello';
 
     protected function configure()
@@ -22,19 +21,9 @@ class SayHelloCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ... put here the code to run in your command
         $string = $input->getArgument('string');
-        $output->writeln('Hello ' . $string . PHP_EOL);
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        $output->writeln('Hello ' . $string);
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
     }
 }
 
