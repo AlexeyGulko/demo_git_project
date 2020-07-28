fe77a62f366d438169226790da7b961be465ce85
Aleksey Gulko <dev.gulkoalexey@gmail.com>
2020-07-28 16:21:47 +0300

    create SayHello console command

--- App.php
+++ App.php
@@ -6,4 +6,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 
 $app = new Application('Text transformer');
 
+$app->add(new App\SayHelloCommand());
+
 $app->run();
